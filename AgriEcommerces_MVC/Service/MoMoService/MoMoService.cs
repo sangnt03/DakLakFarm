85aff2b9ac6e8839365e82fab9cd023780c26772
sangnt03 <sangsosad0905@gmail.com>
2025-11-24 21:07:58 +0700

    delete comment in momoservice

--- AgriEcommerces_MVC/Service/MoMoService/MoMoService.cs
+++ AgriEcommerces_MVC/Service/MoMoService/MoMoService.cs
@@ -23,14 +23,11 @@ namespace AgriEcommerces_MVC.Service.MoMoService
             string returnUrl = _configuration["MoMo:ReturnUrl"];
             string notifyUrl = _configuration["MoMo:NotifyUrl"];
 
-            // requestId bắt buộc là duy nhất cho mỗi request
             string requestId = Guid.NewGuid().ToString();
             string orderIdStr = orderId.ToString();
-            string amountStr = ((long)amount).ToString(); // MoMo yêu cầu số nguyên (VND)
-            string extraData = ""; // Có thể encode base64 thông tin thêm nếu cần
+            string amountStr = ((long)amount).ToString();
+            string extraData = "";
 
-            // Tạo chữ ký HMAC SHA256 (Đúng thứ tự alphabet các tham số)
-            // format: accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
             string rawHash = $"accessKey={accessKey}&amount={amountStr}&extraData={extraData}&ipnUrl={notifyUrl}&orderId={orderIdStr}&orderInfo={orderInfo}&partnerCode={partnerCode}&redirectUrl={returnUrl}&requestId={requestId}&requestType=captureWallet";
 
             string signature = ComputeHmacSha256(rawHash, secretKey);
